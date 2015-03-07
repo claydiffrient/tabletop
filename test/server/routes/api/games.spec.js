77b88967ec7d3ae310094653e37430f9180f4127
Clay Diffrient <cdiffrient@instructure.com>
2015-03-07 13:54:44 -0700

    Adds more tests to the Games API
    - Tests getting a single game
    - Tests getting all the games

--- test/server/routes/api/games.spec.js
+++ test/server/routes/api/games.spec.js
@@ -3,6 +3,12 @@ var supertest = require('supertest');
 var config = require('config');
 var app = require('../../../../server/app');
 var nock = require('nock');
+var mongoose = require('mongoose');
+var mockgoose = require('mockgoose');
+var async = require('async');
+mockgoose(mongoose);
+
+var Game = require('../../../../server/models/game');
 
 nock.enableNetConnect();
 
@@ -15,14 +21,54 @@ var fakeSplendor = {
   description: 'Fake splendor is so fake'
 };
 
+var testGameObjId = mongoose.Types.ObjectId();
+var testGame2ObjId = mongoose.Types.ObjectId();
+
 describe('Games API', function () {
-  before(function () {
+  beforeEach(function (done) {
+    mockgoose.reset();
+
+    async.parallel([
+      function (complete) {
+        Game.create({
+          _id: testGameObjId,
+          title: "testGame",
+          bggId: 11111,
+          thumbnail: "//test.jpg",
+          numPlayers: "2-4",
+          playTime: 30,
+          description: "It's just a test",
+          __v: 0,
+          owners: [ ]
+        }, function (err, model) {
+          complete(err, model);
+        });
+      },
+      function (complete) {
+        Game.create({
+          _id: testGame2ObjId,
+          title: "testGame2",
+          bggId: 11112,
+          thumbnail: "//test2.jpg",
+          numPlayers: "2-6",
+          playTime: 30,
+          description: "It's just another test",
+          __v: 0,
+          owners: [ ]
+        }, function (err, model) {
+          complete(err, model);
+        });
+      }],
+      done
+    );
+
   });
 
-  after(function () {
+  afterEach(function () {
+    mockgoose.reset();
   });
 
-  it('creates a new game given a bggId', function (done) {
+  it('should create a new game given a bggId', function (done) {
     var bgg = nock('http://bgg-json.azurewebsites.net/')
                 .get('/thing/148228')
                 .reply(200, fakeSplendor, {"Content-Type": "application/json"});
@@ -37,5 +83,30 @@ describe('Games API', function () {
         done();
       });
   });
+
+  it('should get a single game', function (done) {
+    supertest(app)
+      .get('/api/v1/games/' + testGameObjId)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw new Error(err);
+        expect(res.body._id).to.be(testGameObjId.toString());
+        done();
+      });
+  });
+
+  it('should get a list of games', function (done) {
+    supertest(app)
+      .get('/api/v1/games/')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw new Error(err);
+        expect(res.body).to.be.an(Array);
+        expect(res.body.length).to.be(2);
+        done();
+      });
+  })
 
 });
