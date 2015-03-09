ba217caa1806c7926f6bd1744d6ff370bf3736c8
Clay Diffrient <cdiffrient@instructure.com>
2015-03-08 20:11:04 -0600

    Removes unnecessary tests from the server routing
    - This change is necessitated by the changes made in
      commit 62eef46

--- test/server/routes/index.spec.js
+++ test/server/routes/index.spec.js
@@ -17,43 +17,4 @@ describe('Server Routing', function () {
         });
     });
   });
-
-  describe('Root Route', function () {
-    it('should render the signin page if not authenticated', function (done) {
-      supertest(app)
-        .get('/')
-        .expect(302)
-        .end(function (err, res) {
-          if (err) throw new Error(err);
-          expect(res.header['location']).to.match(/\/signin/)
-          done();
-        });
-    });
-
-    xit('should open up the vote list if authenticated', function (done) {
-      // Haven't yet figured out how to get this mock to work.
-      passportMock(app);
-
-      supertest(app)
-        .get('/login/test/1')
-        .expect(200)
-        .end(function (err, res) {
-          console.log(res);
-          if (err) throw new Error(err);
-          supertest(app)
-            .get('/')
-            .expect('Content-Type', /html/)
-            .expect(200)
-            .end(function (err, res) {
-              if (err) throw new Error(err);
-              // console.log(res);
-              expect(res.text).to.match(/Current Votes/)
-              done();
-            });
-
-        })
-
-
-    });
-  });
 });
