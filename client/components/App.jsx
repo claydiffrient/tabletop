fc07f6b27347465bfb9f3d9db50dd098d70a5a97
Clay Diffrient <cdiffrient@instructure.com>
2015-04-05 16:36:23 -0600

    Adds global navigation

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import flux from '../flux';
 import Router from 'react-router';
-const { RouteHandler } = Router;
+import { Navbar, Nav } from 'react-bootstrap';
+import { NavItemLink } from 'react-router-bootstrap';
+const { RouteHandler, Link } = Router;
 
 class App extends React.Component {
 
@@ -10,14 +12,26 @@ class App extends React.Component {
   }
 
   render() {
+    var BrandComponent = <Link to="index">TableTop</Link>;
+
     return (
-      <div className="container-fluid">
-        <div className="row center-xs">
-          <div className="col-xs">
-            <h1>TableTop Game Selector</h1>
+      <div>
+        <Navbar brand={BrandComponent}>
+          <Nav>
+            <NavItemLink to="index">Vote List</NavItemLink>
+            <NavItemLink to="games">Game List</NavItemLink>
+          </Nav>
+
+
+        </Navbar>
+        <div className="container-fluid">
+          <div className="row center-xs">
+            <div className="col-xs">
+              <h1>TableTop Game Selector</h1>
+            </div>
           </div>
+          <RouteHandler />
         </div>
-        <RouteHandler />
       </div>
     );
   }
