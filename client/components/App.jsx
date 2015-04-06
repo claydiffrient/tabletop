bd1e02ff03d3d0905123c812a53ff6b2c5a9f3f4
Clay Diffrient <cdiffrient@instructure.com>
2015-04-05 22:12:04 -0600

    Adds login information to the global nav

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,46 @@
 import React from 'react';
 import flux from '../flux';
 import Router from 'react-router';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, DropdownButton, MenuItem } from 'react-bootstrap';
 import { NavItemLink } from 'react-router-bootstrap';
 const { RouteHandler, Link } = Router;
 
 class App extends React.Component {
 
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      user: ENV.user
+    };
+  }
+
   getChildContext() {
     return { flux };
+  }
+
+  renderLoginButton() {
+    return (
+      <NavItem href='/auth/slack'>
+        <img className="GlobalNav__LoginButton-Logo" src="/images/slack_sticker.png" />
+        Login with Slack
+      </NavItem>
+    );
+  }
+
+  renderLoggedInAsButton() {
+    return (
+      <DropdownButton title={"You are logged in as " + this.state.user.slackName}>
+        <MenuItem href="/auth/logout">Logout</MenuItem>
+      </DropdownButton>
+    );
+  }
+
+  renderAccountArea() {
+    if (!this.state.user) {
+      return this.renderLoginButton()
+    } else {
+      return this.renderLoggedInAsButton();
+    }
   }
 
   render() {
@@ -21,8 +53,9 @@ class App extends React.Component {
             <NavItemLink to="index">Vote List</NavItemLink>
             <NavItemLink to="games">Game List</NavItemLink>
           </Nav>
-
-
+          <Nav navbar right>
+            {this.renderAccountArea()}
+          </Nav>
         </Navbar>
         <div className="container-fluid">
           <div className="row center-xs">
