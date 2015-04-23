cca9566ed29134c85e058d02deec57fbe4e6123c
Clay Diffrient <cdiffrient@instructure.com>
2015-04-22 19:29:56 -0600

    Begins conversion to newforms

--- client/components/AddGame.jsx
+++ client/components/AddGame.jsx
@@ -3,6 +3,17 @@ import React from 'react';
 import Router from 'react-router';
 import _ from 'lodash';
 import toastr from 'toastr';
+import forms from 'newforms';
+import newformsBS from 'newforms-bootstrap';
+
+let AddGameForm = forms.Form.extend({
+  bggId: forms.IntegerField(),
+  title: forms.CharField(),
+  thumbnail: forms.URLField(),
+  numPlayers: forms.CharField(),
+  timeToPlay: forms.IntegerField(),
+  description: forms.CharField({widget: forms.Textarea})
+});
 
 class AddGame extends React.Component {
 
@@ -66,95 +77,24 @@ class AddGame extends React.Component {
     });
   }
 
+  renderValidation (field) {
+    if (this.state.hasAttemptedSubmission) {
+      if (this.state.validItems[field]) {
+        return (<span className="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>);
+      } else {
+        return (<span className="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>);
+      }
+    } else {
+      return null;
+    }
+  }
+
   render () {
     return (
-      <div>
-        <div className="row center-xs">
-          <div className="col-xs-6">
-            <h2>Add A Game</h2>
-          </div>
-        </div>
-        <div className="AddGame__Instructions row">
-          <div className="col-xs-12 alert alert-info">
-            You can fill out everything, but if you include a BoardGameGeek Id
-            then the second set of fields will be overriden by the data from
-            BoardGameGeek.com
-          </div>
-        </div>
-        <div className="AddGame__Form-Container row center-xs">
-          <div className="col-xs-10">
-            <form ref="addGameForm" className="AddGame__Form" onSubmit={this.handleSubmit}>
-              <div className="row center-xs">
-                <div className="col-xs-2">
-                  <label htmlFor="bggId">BGG Id:</label>
-                </div>
-                <div className="col-xs-4">
-                  <input className="form-control" type="text" ref="bggId" id="bggId" name="bggId" />
-                </div>
-              </div>
-              <hr />
-              <div className="row center-xs">
-                <div className="col-xs-2">
-                  <label htmlFor="title">Game Title:</label>
-                </div>
-                <div className="col-xs-4">
-                  <input className="form-control" type="text" ref="title" id="title" name="title" />
-                </div>
-              </div>
-              <div className="row center-xs">
-                <div className="col-xs-2">
-                  <label htmlFor="thumbnail">Game Thumbnail:</label>
-                </div>
-                <div className="col-xs-4">
-                  <input className="form-control" type="text" ref="thumbnail" id="thumbnail" name="thumbnail" />
-                </div>
-              </div>
-              <div className="row center-xs">
-                <div className="col-xs-2">
-                  <label htmlFor="numPlayers">Number of Players:</label>
-                </div>
-                <div className="col-xs-4">
-                  <input className="form-control" placeholder="Min-Max" type="text" ref="numPlayers" id="numPlayers" name="numPlayers" />
-                </div>
-              </div>
-              <div className="row center-xs">
-                <div className="col-xs-2">
-                  <label htmlFor="playTime">Time to Play:</label>
-                </div>
-                <div className="col-xs-4">
-                  <input className="form-control" type="text" ref="playTime" id="playTime" name="playTime" />
-                </div>
-              </div>
-              <div className="row center-xs">
-                <div className="col-xs-2">
-                  <label htmlFor="description">Description:</label>
-                </div>
-                <div className="col-xs-4">
-                  <textarea className="form-control" ref="description" id="description" name="description" />
-                </div>
-              </div>
-              <hr />
-              <div className="row center-xs">
-                <div className="col-xs-2">
-                  <label htmlFor="available">At my desk: </label>
-                </div>
-                <div className="col-xs-4">
-                  <input type="checkbox" className="form-control" ref="available" id="available" name="available" />
-                </div>
-              </div>
-              <hr />
-              <div className="row center-xs">
-                <div className="col-xs-2">
-                  <button type="submit" className="AddGame__SubmitButton btn btn-primary">Submit</button>
-                </div>
-                <div className="col-xs-2">
-                  <button type="reset" className="btn">Reset</button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+      <form className="AddGame__Form" onSubmit={this.handleSubmit}>
+      <forms.RenderForm form={AddGameForm} ref="addGameForm">
+      </forms.RenderForm>
+      </form>
     );
   }
 }
