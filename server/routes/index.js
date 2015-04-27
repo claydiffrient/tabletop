488989afde860775ffc4a71f5a1582ccbcfe9cd7
Clay Diffrient <cdiffrient@instructure.com>
2015-04-26 22:17:36 -0600

    Removes old signin page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
-router.get('/signin', function (req, res) {
-  res.render('login');
-});
-
 /* GET All routes, let react-router handle routing */
-router.get('*', function(req, res) {
+router.get('*', function (req, res) {
   var user = req.user || '';
   res.render('home', {
     user: JSON.stringify(user)
