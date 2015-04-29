a89daa89582353a303aad0bd0a6a10991bb577f1
Clay Diffrient <cdiffrient@instructure.com>
2015-04-28 21:48:55 -0600

    Adds more tests around adding a game

--- client/components/__tests__/AddGame.spec.js
+++ client/components/__tests__/AddGame.spec.js
@@ -9,9 +9,11 @@ var helpers = require('./utils/helpers');
 
 describe('AddGame Component', function () {
   var component;
+  var form;
 
   before(function () {
     component = helpers.renderComponent(ComponentToTest).refs.componentUnderTest;
+    form = component.refs.addGameForm.getForm();
   });
 
   after(function () {
@@ -25,6 +27,26 @@ describe('AddGame Component', function () {
   it('should have the submit button disabled on initial load', () => {
     let btn = component.refs.submitBtn.getDOMNode();
     expect(btn.disabled).to.be(true);
+  });
+
+  it('should have the submit button not disabled when a bggId has been entered', () => {
+    form.reset();
+    form.updateData({bggId: 123456});
+    let btn = component.refs.submitBtn.getDOMNode();
+    expect(btn.disabled).to.be(false);
+  });
+
+  it('should have the submit button valid when all fields other than bggId are filled out', () => {
+    form.reset();
+    form.updateData({
+      title: 'Test',
+      thumbnail: 'http://test.test',
+      numPlayers: '2-3',
+      timeToPlay: 60,
+      description: 'testing'
+    });
+    let btn = component.refs.submitBtn.getDOMNode();
+    expect(btn.disabled).to.be(false);
   });
 
   it('should be in a loading state after submission', () => {
