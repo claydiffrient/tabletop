d12b0c6e5eba0ab787df0c79074325c921febf7c
Clay Diffrient <cdiffrient@instructure.com>
2015-05-03 17:26:01 -0600

    Adds initial integration for Rollbar error reporting

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var config = require('config');
 
 /* GET All routes, let react-router handle routing */
 router.get('*', function (req, res) {
   var user = req.user || '';
   res.render('home', {
-    user: JSON.stringify(user)
+    user: JSON.stringify(user),
+    env: process.env.NODE_ENV,
+    rollbarToken: config.get('Rollbar.clientToken')
   });
 });
 
