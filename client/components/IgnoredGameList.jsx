65ca60695676c61b5f74cf9d1317710e898dadcb
Clay Diffrient <cdiffrient@instructure.com>
2015-05-10 07:44:47 -0600

    Adds the IgnoredGameList component

---
+++ client/components/IgnoredGameList.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import UserAPIUtils from '../utils/UserAPIUtils';
+import _ from 'lodash';
+
+class IgnoredGameList extends React.Component {
+  constructor (props, context) {
+    super(props, context);
+    this.state = this.getStateFromStores();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  getStateFromStores () {
+    let stores = this.context.flux.stores;
+    return {
+      games: stores.games.getAllGames(),
+      user: {
+        ignoredGames: stores.users.getIgnoredGames()
+      }
+    };
+  }
+
+  setStateFromStores () {
+    this.setState(this.getStateFromStores());
+  }
+
+  componentWillMount () {
+    UserAPIUtils.getAllIgnoredGames({userId: this.props.userId});
+  }
+
+  componentDidMount () {
+    let stores = this.context.flux.stores;
+    stores.users.addListener('change', this.onChange);
+    stores.games.addListener('change', this.onChange);
+  }
+
+  componentWillUnmount () {
+    let stores = this.context.flux.stores;
+    stores.users.removeListener('change', this.onChange);
+    stores.games.removeListener('change', this.onChange);
+  }
+
+  onChange () {
+    let state = this.getStateFromStores();
+    this.setState(state);
+  }
+
+  handleUnIgnoreClick (gameToUnIgnore, event) {
+    event.preventDefault();
+    this.context.flux.actions.users.unIgnoreGame(gameToUnIgnore._id, this.props.userId);
+  }
+
+  renderGames () {
+    return this.state.user.ignoredGames.map((game) => {
+      let foundGame = _.find(this.state.games, (fGame) => {
+        return fGame._id === game;
+      });
+      return (
+        <li className="IgnoredGameList__Game">
+          {foundGame.title}
+          <button className="IgnoredGameList__Game-Unignore btn btn-xs btn-primary"
+                  type="button"
+                  title="Click to unignore this game."
+                  onClick={this.handleUnIgnoreClick.bind(this, foundGame)}
+          >
+          Unignore
+          </button>
+        </li>
+      );
+    });
+  }
+
+  render () {
+    return (
+      <div className="IgnoredGameList">
+        IgnoredGameList
+        <ul className="IgnoredGameList__List" >
+          {this.renderGames()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+IgnoredGameList.propTypes = {
+  userId: React.PropTypes.string.isRequired
+};
+
+IgnoredGameList.contextTypes = {
+  flux: React.PropTypes.object
+};
+
+export default IgnoredGameList;
