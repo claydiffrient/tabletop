cb5e8c49e5cccc6827e70a811bc7e3e8dc4251cd
Clay Diffrient <clay.diffrient@gmail.com>
2015-10-16 22:51:20 -0600

    Start reverting back to Mongo/Mongoose

--- src/server/models/Game.js
+++ src/server/models/Game.js
@@ -1,30 +1,25 @@
-import Waterline from 'waterline';
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+let { Schema, model } = mongoose;
 
-export default Waterline.Collection.extend({
-  identity: 'game',
-  connection: 'default',
-  attributes: {
-    bggId: {
-      type: 'integer',
-      index: true,
-      unique: true,
-      required: true
-    },
-    title: {
-      type: 'string',
-      unique: true,
-      required: true,
-      index: true
-    },
-    ignoredByUsers: {
-      collection: 'user',
-      via: 'ignoredGames'
-    },
-    thumbnailUrl: {type: 'string'},
-    minPlayers: {type: 'integer'},
-    maxPlayers: {type: 'integer'},
-    description: {type: 'string'},
-    mechanics: {type: 'array'},
-    playTime: {type: 'integer'}
-  }
+let GameSchema = new Schema({
+  title: { type: String, unique: true, required: true },
+  owners: [{
+    owner: {type: Schema.Types.ObjectId, ref: 'User'},
+    available: Boolean,
+    approved: Boolean,
+    approvalHash: String
+  }],
+  bggId: { type: Number, unique: true },
+  thumbnail: String,
+  numPlayers: String,
+  playTime: Number,
+  description: String,
+  mechanics: [String]
 });
+
+GameSchema.plugin(uniqueValidator);
+
+let GameModel = model('Game', GameSchema);
+
+export default GameModel;
--- src/test/server/models/Game.spec.js
+++ src/test/server/models/Game.spec.js
@@ -1,52 +1,11 @@
 /* eslint-env node, mocha */
-import expect from 'expect.js';
-import Waterline from 'waterline';
-import sailsMemoryAdapter from 'sails-memory';
+/* eslint-env node, mocha */
+import { expect } from 'chai';
+import Game from '../../../server/models/Game';
 
 describe('GameModel', () => {
-  let waterline = new Waterline();
-  let config = {
-    adapters: {
-      'memory': sailsMemoryAdapter
-    },
-    connections: {
-      default: {
-        adapter: 'memory'
-      }
-    }
-  };
-
-  before((done) => {
-    let gameModel = require('../../../server/models/Game.js');
-    let userModel = require('../../../server/models/User.js');
-    waterline.loadCollection(userModel);
-    waterline.loadCollection(gameModel);
-    waterline.initialize(config, (err, collections) => {
-      if (err) return done(err);
-      done();
-    });
-  });
-
-  after(() => {
-    let adapters = config.adapters || {};
-    let promises = [];
-
-    Object.keys(adapters)
-          .forEach((adapter) => {
-            if (adapters[adapter].teardown) {
-              let promise = new Promise((resolve) => {
-                adapters[adapter].teardown(null, resolve);
-              });
-              promises.push(promise);
-            }
-          });
-    return Promise.all(promises);
-  });
-
   it('should be able to create a new game', () => {
-    let Game = waterline.collections.game;
-
-    let testGame = Game.create({
+    let testGame = new Game({
       bggId: 123,
       title: 'Game of Tests',
       thumbnailUrl: 'http://example.com/image.png',
@@ -57,44 +16,6 @@ describe('GameModel', () => {
       playTime: 2
     });
 
-    testGame.then((game) => {
-      expect(game.title).to.be('Game of Tests');
-    });
-  });
-
-  xit('should be able to associate a user with the game', () => {
-    let Game = waterline.collections.game;
-    let User = waterline.collections.user;
-
-    User.create({
-      username: 'tester2015',
-      firstName: 'Test',
-      lastName: 'McTester',
-      email: 'tester2015@example.com',
-      password: 'password12345'
-    })
-    .then((user) => {
-      Game.create({
-        bggId: 120,
-        title: 'Game of Tests 2',
-        thumbnailUrl: 'http://example.com/image.png',
-        minPlayers: 1,
-        maxPlayers: 10,
-        description: 'A simple game for testing things',
-        mechanics: ['Deck Building', 'Test Taking'],
-        playTime: 2
-      })
-      .then((game) => {
-        console.log(game);
-        game.owners.add(user);
-
-        return game.save();
-      })
-      .then(() => {
-        return Game.find().populate('owners');
-      })
-      .then(console.log)
-      .catch(console.error);
-    });
+    expect(testGame.title).to.be('Game of Tests');
   });
 });
