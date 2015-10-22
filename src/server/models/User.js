a4b05779d775864c3e2c903b45cd24df6c2f8535
Clay Diffrient <clay.diffrient@gmail.com>
2015-10-21 22:26:33 -0600

    Add naive password hashing and spec

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+
 export default function (mongoose) {
   let Schema = mongoose.Schema;
 
@@ -10,6 +12,16 @@ export default function (mongoose) {
       type: String,
       match: [/.+\@.+\..+/, 'Please fill a valid e-mail address']
     }
+  });
+
+  UserSchema.pre('save', function (next) {
+    var user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.hash(user.password, 8, function (err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
   });
 
   return mongoose.model('User', UserSchema);
--- src/test/server/models/User.spec.js
+++ src/test/server/models/User.spec.js
@@ -7,7 +7,7 @@ mockgoose(mongoose);
 import UserModel from '../../../server/models/User';
 mongoose.connect('mongodb://localhost/TestingDB-58');
 
-let Game = UserModel(mongoose);
+let User = UserModel(mongoose);
 
 describe('UserModel', () => {
   beforeEach(() => {
@@ -15,14 +15,30 @@ describe('UserModel', () => {
   });
 
   it('should be able to create a new user', (done) => {
-    Game.create({
+    User.create({
       username: 'tester2015',
       firstName: 'Test',
       lastName: 'McTester',
       email: 'tester2015@example.com',
       password: 'password12345'
-    }, (err) => {
+    }, (err, user) => {
       expect(err).to.not.be.ok;
+      expect(user.username).to.equal('tester2015');
+      done();
+    });
+  });
+
+  it('should not store the password in plain text', (done) => {
+    let unencrypted = 'password12345';
+    User.create({
+      username: 'tester2015',
+      firstName: 'Test',
+      lastName: 'McTester',
+      email: 'tester2015@example.com',
+      password: unencrypted
+    }, (err, user) => {
+      expect(err).to.not.be.ok;
+      expect(user.password).to.not.equal(unencrypted);
       done();
     });
   });
